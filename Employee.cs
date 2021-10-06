91fddbff03709461da0b2307066ec90b6921e116
JohnDoe13 <nick.alzen@live.com>
2021-10-07 00:16:26 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staff_Salary_Report
+{
+    public class Employee
+    {
+        //public int NumOfStaff { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public void PrintEmployee()
+        {
+            Console.WriteLine($"Name {Name}, Salary {Salary}.");
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Staff_Salary_Report
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            var emp = new Employee();
+
+            
+            Console.WriteLine("How many employees?");
+            num = int.Parse(Console.ReadLine());
+           // Console.WriteLine(num);
+
+            string[] sname = new string[num];
+            int [] isalary=new int [num];
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Name of employee?");
+                            emp.Name = Console.ReadLine();
+                sname[i] = emp.Name;
+                            
+                Console.WriteLine("Salary?");
+                            emp.Salary = int.Parse(Console.ReadLine());
+                isalary[i] = emp.Salary;
+                
+
+                //Console.WriteLine(myname);
+                
+                
+               
+            }
+
+            for (int j = 0; j < num; j++)
+            {
+                //Console.WriteLine($"Name {emp.Name}, Salary {emp.Salary}");
+                Console.WriteLine($"Name {sname[j]}, Salary {isalary[j]}.");
+               
+
+            }
+
+            //string fname = Console.WriteLine("Enter your name:");
+
+
+
+
+
+
+
+        }
+    }
+}
